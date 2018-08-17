9e6fbc1e2519097d0139a4ccdeb5f63dcd0d027a
thorhan <paezvallejohecthor@gmail.com>
2018-08-17 11:53:01 -0500

    banexpunico

--- laravel/app/models/Reporte.php
+++ laravel/app/models/Reporte.php
@@ -682,7 +682,7 @@ class Reporte extends Eloquent
                         LEFT JOIN rutas_detalle rd ON re.ruta_detalle_id=rd.id 
                         LEFT JOIN areas a ON rd.area_id=a.id  
                         LEFT JOIN doc_digital_temporal dd ON re.doc_digital_id = dd.id $qExtra
-                        WHERE re.tabla_relacion_id='".$referido->tabla_relacion_id."'
+                        WHERE re.estado=1 and re.tabla_relacion_id='".$referido->tabla_relacion_id."'
                         UNION
                         SELECT re.ruta_id,re.ruta_detalle_id,sustento,fecha_hora_sustento fecha_hora,f.nombre proceso,a.nombre area,rd.norden,'s' tipo,null as doc_digital_id
                         FROM sustentos s
@@ -691,6 +691,7 @@ class Reporte extends Eloquent
                         LEFT JOIN areas a ON rd.area_id=a.id  
                         INNER JOIN rutas r ON re.ruta_id=r.id 
                         INNER JOIN flujos f ON r.flujo_id=f.id
+                        WHERE s.estado = 1 and re.estado = 1
                         ORDER BY ruta_id,norden,tipo";
                 $r=DB::select($sql);
                 return $r;                
