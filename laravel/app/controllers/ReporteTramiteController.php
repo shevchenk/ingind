5b8cae5942b39553fd8c522cc241c9be3f3fddd1
Jhoubert Rincon <jhoubert@veflat.com>
2018-10-02 16:22:59 -0500

    add video expediente

--- laravel/app/controllers/ReporteTramiteController.php
+++ laravel/app/controllers/ReporteTramiteController.php
@@ -14,7 +14,11 @@ class ReporteTramiteController extends BaseController
         }
       }
       
+
+
       $r = ReporteTramite::TramiteUnico( $array );
+
+
       return Response::json(
           array(
               'rst'=>1,
@@ -44,6 +48,45 @@ class ReporteTramiteController extends BaseController
     public function postExpedienteunico()
     {
       $rst=ReporteTramite::ExpedienteUnico(); 
+
+
+       $new = array();
+      
+        $ftp_server = "10.0.100.11";
+        $conn_id = ftp_connect($ftp_server);
+        $login_result = ftp_login($conn_id, 'anonymous', '');
+        $contents = ftp_nlist($conn_id, ".");
+        $new=array();
+        foreach ($contents as $key => $value) {
+          $new[$key]=array('server'=>"10.0.100.11",'nombre'=>utf8_decode($value);)
+        }
+
+        $ftp_server = "10.0.1.61";
+        $conn_id = ftp_connect($ftp_server);
+        $login_result = ftp_login($conn_id, 'anonymous', '');
+        $contents = ftp_nlist($conn_id, ".");
+        foreach ($contents as $key => $value) {
+          $new[$key]=array('server'=>"10.0.1.61",'nombre'=>utf8_decode($value);)
+        }
+
+        foreach ($rst as $ind => $ndc) {
+                $ad=explode(" - ", $ndc->referido);
+                if(isset($ad[1]))
+                foreach ($new as $iFile => $dFile) {
+
+                        $daFile=strtolower(str_replace(' ', '', $dFile['nombre']));
+                        $nom = strtolower(str_replace(' ', '', $ad[0]));
+                        $num = (int)str_replace("Nº ", '', $ad[1]);
+
+                        $c1 = strpos($daFile, $nom);
+                        $c2 = strpos($daFile, "".$num);
+                        if($c1 !== false && $c2 !== false){
+                                //echo "FOUND: $ndc->referido -> ".$new[$iFile];
+                          $rst[$ind]->referido .= ' <b><a href="javascript:loadVid('.($ind+1).');"<i class="fa fa-video-camera"><input type="hidden" id="vid_'.($ind+1).'" value="'.$dFile['nombre'].'"> </i></a></b>';
+                        }
+                }
+        }
+
       return Response::json(
             array(
                 'rst'=>1,
