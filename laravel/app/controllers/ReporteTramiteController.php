983ff9bed4c02a9005a45b3ba0bfe4bc1b0897c3
Jhoubert Rincon <jhoubert@veflat.com>
2018-10-03 17:25:44 -0500

    t

--- laravel/app/controllers/ReporteTramiteController.php
+++ laravel/app/controllers/ReporteTramiteController.php
@@ -100,7 +100,7 @@ class ReporteTramiteController extends BaseController
                 }
         }
 
-        var_dump($rst);die();
+        ///var_dump($rst);die();
 
       return Response::json(
             array(
