3328a7eef33aa2c39768125052b89b32745ad341
Jhoubert Rincon <jhoubert@veflat.com>
2018-10-05 08:17:25 -0500

    Multiple servers fix

--- laravel/app/controllers/ReporteTramiteController.php
+++ laravel/app/controllers/ReporteTramiteController.php
@@ -70,7 +70,7 @@ class ReporteTramiteController extends BaseController
 
     function getFilesR($conn_id,$path='/',$srv){
       $result = array();
-      $list = ftp_rawlist($conn_id, $path, TRUE);
+      $list = @ftp_rawlist($conn_id, $path, TRUE);
         if(is_array($list))foreach($list as $ind => $val){
             $x = explode(' ',$val);
             $i=3;
