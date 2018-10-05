f58346a0b6bfef33b10dc5fc7e6588c8deddaa21
Jhoubert Rincon <jhoubert@veflat.com>
2018-10-05 09:58:33 -0500

    x

--- laravel/app/models/RutaDetalle.php
+++ laravel/app/models/RutaDetalle.php
@@ -85,7 +85,7 @@ class RutaDetalle extends Eloquent
                     "=>",
                     IF(rdv.condicion=0,"NO",CONCAT("+",rdv.condicion) ),
                     "=>",
-                    IFNULL(concat(rdv.documento,\'_\',rdv.doc_digital_id),""),
+                    IFNULL(rdv.doc_digital_id,""),
                     "=>",
                     IFNULL(rdv.observacion,""),
                     "=>",
