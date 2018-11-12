c691cf6fbdff84765963c967cebca111d760967b
Jhoubert Rincon <jhoubert@veflat.com>
2018-11-12 14:45:19 -0500

    arreglado orden de licencias

--- laravel/app/controllers/FormatoLicenciaController.php
+++ laravel/app/controllers/FormatoLicenciaController.php
@@ -20,7 +20,7 @@ class FormatoLicenciaController extends \BaseController
             //$cargos = FormatoLicenciaContruccion::get(Input::all());
             $sql = "SELECT lc.* 
                 FROM licencia_construccion lc
-                    WHERE estado = 1;";        
+                    WHERE estado = 1 ORDER BY id DESC;";        
             $cargos = DB::select($sql);
 
             return Response::json(array('rst'=>1,'datos'=>$cargos));
